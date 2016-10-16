397337462d17818d0b3937272228fd04346673cc
joseph valentine <josephvalentine40@gmail.com>
2016-10-16 14:09:35 -0600

    can read files in now, trying to fix 2d to 1d arrays

--- dp2/main.c
+++ dp2/main.c
@@ -3,12 +3,14 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 #define numberToFit 6
 #define min_Val .01
 #define printDebug 1
+#define maxCharLength 1000
+#define maxNumberOfLists 100
+#define runAllLists 0 //if 0 it will only run the first list in the input file
+
 int numberOfFlips = 0;
 int curMaxLoc;
 int curMinLoc;
@@ -16,7 +18,7 @@ char *StringOut;
 bool forwardSearch;
 int sortingArraySize;
 
- addintToStr(char * list, int inputVal){ 
+char * addintToStr(char * list, int inputVal){ 
 	char buffer[10]; 
 	snprintf(buffer,10,"%d ", inputVal); 
 	strcat(list, buffer);
@@ -200,12 +202,63 @@ void findNextFlip(int array_input[], int array_sort[], int curSearchInt, char * 
 	
 }
 
+int readInputFile(int arrays_to_organize[maxNumberOfLists][maxCharLength], char * filename){
+	FILE *stream;
+	stream = fopen(filename, "r");
 
+  	//read file
+   if (stream){
 
-int main(void)
+    int lineCount = 0;
+    char line[32];
+    while(fgets(line, 32, stream)){
+	 	char * pch;
+	 	//printf ("Splitting string \"%s\" into tokens:\n", line);
+	 	int rowCount = 0;
+	 	pch = strtok (line," ");
+
+		while (pch != NULL)
+	  	{
+	    	//printf ("%s\n",pch);
+	    	//printf ("linecount:%i ,rowCount:%i\n",lineCount, rowCount);
+	   		int num = atoi(pch);
+	   		// printf("opp there it is: %i\n", thing);
+	   		 arrays_to_organize[lineCount][rowCount] = num;
+	    	//printf("worked\n");
+	    
+	    	rowCount++;
+	    	pch = strtok (NULL, " \r\n");
+	  	}
+    lineCount++; 
+    }
+    fclose(stream);
+    return 1;
+  } else {
+  	printf("\nunable to open file\n");
+  	return 0;
+  }
+}
+void arraysToArray(int arrays_to_organize[maxNumberOfLists][maxCharLength], int array_to_Organize[], int stackNum){
+	int i = 0;
+	while(arrays_to_organize[stackNum][i] != 0){
+		arrays_to_organize[i] = arrays_to_organize[stackNum][i];
+	}
+	return;
+}
+int main(int argc, char *argv[])
 {
+	if (argc < 2)
+    {
+     printf("\nUsage: -name\n name.txt: the name of the textfile\n");
+     exit(0);
+   }
+  	//init some vars
+   	char * filename = argv[1];
+	int arrays_to_organize[maxNumberOfLists][maxCharLength];
 
+	if(!readInputFile(arrays_to_organize, filename)) return 0;
 	
+	//int array_to_Organize[] = arrays_to_organize[1];
 	int array_to_Organize[] = {2, 5, 1, 4, 3 };
 	//int array_to_Organize[] = {3, 5, 10, 4, 7, 2, 9, 1, 6, 8  };
 	//int array_to_Organize[] = {1, 10, 9, 6, 11, 7, 12, 2, 8, 13, 4, 15, 14, 3, 5  };
@@ -218,6 +271,9 @@ int main(void)
 	//int array_to_Organize[] = {73, 46, 20, 17, 32, 43, 71, 53, 41, 59, 69, 39, 49, 63, 48, 37, 18, 29, 30, 56, 42, 7, 27, 14, 6, 10, 26, 8, 19, 24, 13, 50, 25, 79, 15, 3, 16, 57, 65, 52, 72, 28, 35, 78, 21, 77, 34, 33, 5, 45, 61, 76, 54, 4, 36, 74, 64, 40, 60, 80, 11, 12, 51, 75, 2, 9, 31, 67, 22, 66, 55, 1, 68, 47, 23, 58, 38, 70, 62, 44, };
 	//int array_to_Organize[] = {20, 78, 82, 50, 2, 59, 79, 8, 21, 81, 33, 58, 3, 48, 42, 83, 74, 24, 23, 5, 44, 41, 9, 40, 38, 49, 37, 15, 11, 34, 18, 88, 47, 60, 25, 87, 77, 30, 35, 12, 31, 56, 89, 57, 29, 53, 13, 39, 73, 7, 51, 52, 17, 75, 26, 16, 28, 45, 55, 69, 14, 66, 6, 72, 61, 4, 1, 76, 64, 71, 46, 43, 54, 67, 86, 27, 80, 36, 32, 22, 90, 68, 19, 10, 85, 62, 70, 65, 84, 63, };
 	//int array_to_Organize[] = {94, 93, 28, 55, 64, 100, 49, 5, 15, 19, 31, 33, 42, 8, 37, 68, 71, 69, 16, 51, 91, 89, 57, 79, 56, 24, 27, 47, 67, 58, 20, 82, 74, 4, 54, 14, 80, 60, 44, 18, 59, 83, 22, 25, 32, 98, 23, 87, 2, 86, 43, 9, 30, 46, 61, 29, 48, 26, 66, 81, 34, 38, 36, 12, 40, 92, 65, 41, 77, 78, 90, 84, 85, 96, 21, 35, 13, 72, 50, 45, 52, 6, 39, 99, 17, 62, 11, 88, 1, 76, 75, 73, 97, 70, 7, 3, 63, 53, 10, 95,};
+	int y = (int) sizeof(arrays_to_organize);
+	if(printDebug) printf("Length of array: %d\n", y);
+
 
 	int n = (int)( sizeof(array_to_Organize) / sizeof(array_to_Organize[0]) );
 	if(printDebug) printf("Length of array: %d\n", n);
