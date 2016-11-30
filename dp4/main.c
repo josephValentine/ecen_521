485854e6035d15ddaf4e17ece84bf04a4060a4e5
joseph valentine <josephvalentine40@gmail.com>
2016-11-29 23:16:36 -0700

    merge of joe and lorin's code

--- dp4/main.c
+++ dp4/main.c
@@ -1,14 +1,36 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <sys/time.h>
+#include <time.h>
+#include <string.h>
+
 #define NUMSETS 3
 #define MAXSETVALUE 4
+#define PATH "D:\\DPrograms\\CodeLite\\CWorkspace\\DesignProblem4\\datasets\\DataSets\\s-X-12-6.txt"
+#define READFROMFILE 0
 
-int setLengths[NUMSETS]={0}; //store the length of each given set
-bool setCoverMatrix[NUMSETS][MAXSETVALUE]={false}; //store which values are included in each set
-int valueMultiplicity[MAXSETVALUE]={0}; //store how often a value occurs given each value in each subset
-bool currSet[MAXSETVALUE]={0}; //which values do we have in our concatenated set?
-bool universe[4]={false, true, true, true}; //which values (represented by their indexes) do we need
+int characterListLength = 1;
+int subsetListLength = 1;
+int SizeOfEachSubset[NUMSETS]={0};//store the length of each given set
+int previousRandVal = 1;
+int subsetArrays[NUMSETS][MAXSETVALUE]={0};
+bool subsetArrayMatrix[NUMSETS][MAXSETVALUE]={false}; //store which values are included in each set
+int totalFromAllSubsets[MAXSETVALUE]={0}; //store how often each integer occurs given each value in each subset this will be usefull for pruning
+bool universe[MAXSETVALUE]; //which values (represented by their indexes) do we need
+
+int myRand(){
+  struct timeval tv;
+  gettimeofday(&tv,NULL);
+  int seed = tv.tv_usec;
+  srand(seed);
+  int x = rand();
+  previousRandVal = 104729*previousRandVal + x;
+  x = previousRandVal % (100);
+  return x;
+}
 
 void printArray(int array[], int size){
   printf("array----------------\n");
@@ -41,25 +63,26 @@ void printArray2(bool array[][MAXSETVALUE]){
   print the solution etc.
 ------------------------------------------------------*/
 void process_solution(int moves[], int k){
-  printf("these are the lists:\n");
+  printf("solution found: these are the lists:\n");
   printArray(moves,NUMSETS);
 }
 
 /*-----------------------------------------------------
-  test whether the current solution is valid
+  test whether the current solution is valid. there may 
+  be a more efficient way of doing this...
 ------------------------------------------------------*/
 bool is_a_solution(int moves[], int k, int n){
-  printf("in is_a_solution, k is:%d\n",k);
-  printArray(moves,k+1);
-  boolprintArray(universe,4);
+  // printf("in is_a_solution, k is:%d\n",k);
+  // printArray(moves,k+1);
+  // boolprintArray(universe,4);
   bool solution;
   for(int i=0; i < MAXSETVALUE; i++){ //for all values that may be needed in universal set
     if(universe[i]){//we need this value in our list of sets
       solution = false;//guilty till proven otherwise
-      for(int j=0; j < k; j++){ //for each move we have made (each set we have chosen)
+      for(int j=0; j <= k; j++){ //for each move we have made (each set we have chosen)
         int setId = moves[j];
-        //printf("setID %d\n", setId);
-        if(setCoverMatrix[setId][i]){//we have the required value in a chosen set
+        //printf("setID %d for move: %d\n", setId, j);
+        if(subsetArrayMatrix[setId][i]){//we have the required value in a chosen set
           solution = true;
         }
       }
@@ -72,30 +95,23 @@ bool is_a_solution(int moves[], int k, int n){
 
 /*-----------------------------------------------------
   order the recursion. i think this is where we will 
-  want to call our pruning "checker" functions.
+  want to call our pruning "checker" functions. right now
+  it will just recurse down with any set that we havent
+  already added yet
 ------------------------------------------------------*/
 void construct_candidates(int moves[], int k, int n, int candidates[], int *numCandidates){
-    printf("in construct_candidates, k is:%d\n",k);
-
-  // printf("in construct_candidates\n");
-  // *numCandidates = 0;
-  // for (int i=1; i<=n; i++){
-  //     candidates[ *numCandidates] = i;
-  //     *numCandidates = *numCandidates + 1;
-  //}
+   // printf("in construct_candidates, k is:%d\n",k);
 
   int i;                  
   bool used[NUMSETS];          
   for (i=0; i<NUMSETS; i++){
     used[i] = false;
   } 
-  printf("here1\n");
   for (i=0; i<k; i++){
      //printf("k:%d\n",k);
      used[ moves[i] ] = true;//mark the sets we have already used
   }
 
-  printf("here\n");
   *numCandidates = 0;
   for (i=0; i<n; i++){
     if (used[i] == false) {
@@ -107,13 +123,17 @@ void construct_candidates(int moves[], int k, int n, int candidates[], int *numC
 }
 
 /*-----------------------------------------------------
-  main recursive function
+  main recursive function:
+  moves[]: list of sets selected in current path
+  k: depth
+  n: max depth
 ------------------------------------------------------*/
 
 bool finished = false; /* found all solutions yet? */
 void backtrack(int moves[], int k, int n)
 {
-  printf("level/k: %d\n",k);
+  // sleep(1);
+  // printf("level/k: %d\n",k);
   int candidates[NUMSETS];/* candidates for next position */
   int numCandidates;
   int i;/* counter */
@@ -123,9 +143,11 @@ void backtrack(int moves[], int k, int n)
   } else {
       k = k+1;
       construct_candidates(moves, k, n, candidates, &numCandidates);
-      for (i=0; i<numCandidates; i++) {
-        moves[k] = candidates[i];//next move is next candidate
-        printf("new move is;%d\n",candidates[i]);
+      // printf("candidates\n");
+      // printArray(candidates,numCandidates);
+      for (i=0; i<numCandidates; i++) { 
+        moves[k] = candidates[i];//next move is ordered by construct_candidates
+        // printf("new move is;%d\n",candidates[i]);
         backtrack(moves, k, n);
         
       }
@@ -134,32 +156,97 @@ void backtrack(int moves[], int k, int n)
 }
 
 /*-----------------------------------------------------
-  start the recursion
+  start the recursion.  we require an initial pick
+  of our list of sets
 ------------------------------------------------------*/
 void set_cover(int n){
   //n is the number of set # we have recursed through
-  int moves[NUMSETS]; //moves made to current state
+  int moves[NUMSETS]; //moves made to get to the current state of our solution
   int k=0; //move iterator
   moves[0]=0; //pick the initial move
   backtrack(moves, k, n);
 }
 
+
+int readInputFile(int array[NUMSETS][MAXSETVALUE], char * filename){
+  FILE *stream;
+  stream = fopen(filename, "r");
+
+    //read file
+   if (stream){
+
+    int limiter = 0;
+    int lineCount = 0;
+    char line[MAXSETVALUE];
+    while(fgets(line, MAXSETVALUE, stream)){
+      char * pch;
+      //printf ("Splitting string into tokens:\"%s", line);
+      int rowCount = 0;
+      pch = strtok (line," ");
+      
+      if (limiter < 1){
+        characterListLength = atoi(pch);
+      }
+      if (limiter == 1){
+       subsetListLength = atoi(pch);
+      }
+      if(limiter >1){
+        while (pch != NULL)
+        {
+          //printf ("%s\n",pch);
+          //printf ("linecount:%i ,rowCount:%i\n",lineCount, rowCount);
+          int num = atoi(pch);
+          //if(printout) printf("opp there it is: %i\n", num);
+          array[lineCount][rowCount] = num;
+          //printf("worked\n");
+          totalFromAllSubsets[num-1] = totalFromAllSubsets[num-1] +1;
+          rowCount++;
+          pch = strtok (NULL, " \r\n");
+        //if(BiggestSubset < rowCount) {printf("newLongest:%d\n",rowCount); BiggestSubset = rowCount;}
+        }
+        SizeOfEachSubset[lineCount] = rowCount;
+        lineCount++; 
+      }
+    
+      limiter++;    
+    }
+    fclose(stream); 
+    return 1;
+  } else {
+    printf("\nunable to open file\n");  
+    return 0; 
+  }
+}
+
+
 int main()
 {
-  setLengths[0]=1;
-  setLengths[1]=1;
-  setLengths[2]=2;
-  setCoverMatrix[0][1]=true;
-  setCoverMatrix[1][3]=true;
-  setCoverMatrix[2][1]=true;
-  setCoverMatrix[2][2]=true;
-  valueMultiplicity[1]=2;
-  valueMultiplicity[2]=1;
-  valueMultiplicity[3]=1;
-
-  bool currSet[MAXSETVALUE]; //which values do we have in our concatenated set?
-printArray2(setCoverMatrix);
-  set_cover(5);//3 cause max num of sets...
+  if(READFROMFILE){
+    char * filename = PATH;
+    readInputFile(subsetArrays, filename);
+    //---------
+    //needed:
+    //function to create subsetarraymatrix from subset arrays
+    //---------
+  } else {
+    universe[0]=false;
+    universe[1]=true;
+    universe[2]=true;
+    universe[3]=true;
+    SizeOfEachSubset[0]=1;
+    SizeOfEachSubset[1]=1;
+    SizeOfEachSubset[2]=2;
+    subsetArrayMatrix[0][1]=true;
+    subsetArrayMatrix[1][3]=true;
+    subsetArrayMatrix[2][1]=true;
+    subsetArrayMatrix[2][2]=true;
+    totalFromAllSubsets[1]=2;
+    totalFromAllSubsets[2]=1;
+    totalFromAllSubsets[3]=1;
+  }
+  printf("printing the subset array matrix (row represents the subset, col represents the integer, 1 means it exists . . .\n");
+  printArray2(subsetArrayMatrix);
+  set_cover(NUMSETS);//pass in the max allowable depth
   
   return 0;
 }
